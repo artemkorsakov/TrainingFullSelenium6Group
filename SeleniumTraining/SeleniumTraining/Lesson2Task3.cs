fb1f6076e182a23711ce2d22ed26b63bc1a33918
artemkorsakov@mail.ru <korsakov@ActionDigital.ru>
2017-04-20 16:00:11 +0300

    Выполнение третьего задания второго урока

---
+++ SeleniumTraining/SeleniumTraining/Lesson2Task3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTraining.Pages;
+
+namespace SeleniumTraining
+{
+    [TestFixture]
+    public class Lesson2Task3
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 10);
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            _driver.Url = "http://localhost:8080/litecart/admin/";
+            AdminPage adminPage = new AdminPage(_driver);
+            adminPage.Login("admin", "admin");
+            adminPage.Logout();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
---
+++ SeleniumTraining/SeleniumTraining/Pages/AdminPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumTraining.Pages
+{
+    /// <summary>
+    /// Страница администратора
+    /// </summary>
+    internal class AdminPage
+    {
+        private readonly IWebDriver _driver;
+
+        internal AdminPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        /// <summary>
+        /// Вход на панель администратора под заданными логином и паролем
+        /// </summary>
+        internal void Login(string login, string password)
+        {
+            _driver.FindElement(By.Name("username")).SendKeys(login);
+            _driver.FindElement(By.Name("password")).SendKeys(password);
+            _driver.FindElement(By.XPath("//button[.='Login']")).Click();
+        }
+
+        /// <summary>
+        /// Разлогирование с панели администратора
+        /// </summary>
+        internal void Logout()
+        {
+            _driver.FindElement(By.XPath("//a[@title='Logout']")).Click();
+        }
+    }
+}

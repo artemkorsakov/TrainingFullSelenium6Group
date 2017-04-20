357481e15fb499c3740ac99603ba315985744832
artemkorsakov@mail.ru <korsakov@ActionDigital.ru>
2017-04-20 13:12:17 +0300

    Первое задание

---
+++ SeleniumTraining/SeleniumTraining/Lesson2Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTraining
+{
+    [TestFixture]
+    public class Lesson2Task1
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            _driver.Url = "http://software-testing.ru/forum/";
+            _driver.FindElement(By.Id("main_search")).SendKeys("Проверка поиска");
+            _driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
